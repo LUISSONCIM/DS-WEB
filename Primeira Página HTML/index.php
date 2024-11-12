1dba4bba662dfa2c33856f1d3bdf4d051da8ee51
LUISSONCIM <luiseduardosoncim3@gmail.com>
2024-11-12 16:07:59 -0300

    Aula-07

---
+++ Primeira Página HTML/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $nome = "Luis Eduardo Galera Soncim";
+    $idade = 17;
+    $email = "luiseduardosoncim3@gmail.com";
+
+    echo "nome: $nome "
+    echo "idade: $idade"
+    echo "email: $email"
+?>
+    
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário Login</title>
+</head>
+<body>
+   
+    <h1>Sistema Login</title>
+    <login.php $_POST[usuario]>
+    <senha.php $_POST[senha]>
+
+</body>
+</html>
