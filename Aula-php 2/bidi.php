0203ad8fccc5a45d7aca52de9c7d3cbec84b368f
LUISSONCIM <luiseduardosoncim3@gmail.com>
2024-11-26 14:42:13 -0300

    Décima primeira Commit

---
+++ Aula-php 2/bidi.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "<h1>Atividade 01</h1>";
+$nome[] = "Coca-Cola";
+$preço[] = "9,49.";
+$estoque[] = "1.900";
+
+$Valor total = $preço * $estoque
+$Final = 1.900 * 9,49;
+$resultado = 18.031;
+
+foreach($estoque as $preço => $resultado){
+echo $resultado;
+}
+
+echo "<h1>Atividade 02</h1>";
+
+$alunos = array( 
+    array('nome' => 'Lucas', 'biologia' => 9, 'matematica' => 8 ),
+    array('nome' => 'Bento', 'biologia' => 5, 'matematica' => 7 ),
+    array('nome' => 'Sophia','biologia' => 7, 'matematica' => 6 ),
+
+?>
