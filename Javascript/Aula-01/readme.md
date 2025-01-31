10cdbc665de5036fa248337580cda81886405ccc
AlunoDS3 <luiseduardosoncim3@gmail.com>
2025-01-31 12:20:20 -0300

    Primeiro Commit

---
+++ Javascript/Aula-01/readme.md
@@ -0,0 +1,7 @@
+Aula Introdutoria sobre Lógica de Programação com Javascript
+
+//tentaiva de soma
+
+//Esemplo de Prompt()
+
+//Exemplo de confirm()
---
+++ Javascript/Aula-01/script.js
@@ -0,0 +1,36 @@
+console.log("Ola Mundo");
+
+//xemplo de alert()
+alert("Ola mundo 2")
+
+alert("O que é para comer ?")
+
+alert("Rafael vai virar calvão de cria")
+
+//Exemplo de confirm()
+var teste= confirm("Ola mundo")
+//exibindo a opcao escolhida pelo usuario
+console.log(teste)
+
+//Esemplo de Prompt()
+var testePrompt = prompt("Digite seu nome")
+//exibindo o nome do usuario
+console.log(testePrompt)
+alert("Luis")
+
+
+//tentaiva de soma
+var numero1 = prompt("Digite o primeiro numero")
+var numero2 = prompt("Digite o segundo numero")
+console.log (typeof(numero1))
+alert(numero1 + numero2)
+
+var m = prompt("Digite um valor")
+var n = prompt("Digite outro valor")
+var s = m + n
+window.alert("A resposta é: " +s)
+
+
+
+
+
